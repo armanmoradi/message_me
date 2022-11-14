c666fb0b7cb807c9f7ad116d38df7137a21fe46f
Arman Moradi <armanmoradi8gmail.com>
2022-11-15 01:13:22 +0330

    Display messages realtime

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
     helper_method :current_user, :logged_in?
 
   def current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    protect_from_forgery
     before_action :logged_in_redirect, only: [:new, :create]
     def new
 
